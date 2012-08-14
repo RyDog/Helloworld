ef3fedae46eebb8b51d8c479d45df31b6eac629b
Ryan Melogy <ryan@faithstreet.com>
2012-08-14 15:51:48 -0400

    Commiting all of the Rails Files. -RyDog

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Helloworld::Application.config.secret_token = '369a5806acb661bf0f77d0a0b1b0168428d46df5a276d321b896451b883a33111a766ad6f1530748983d70f844fa956d965047ceb71a6608f8fe45cec15761bc'
